8ceafc68da798fa69f4d816f1139fa3641c458b9
Silvia Reyes <104657695+NathaliaReyes@users.noreply.github.com>
2024-03-14 13:53:18 -0600

    movie_db created conatining 2 tables, movies and reviews

---
+++ db/schema.sql
@@ -0,0 +1,16 @@
+DROP DATABASE IF EXISTS movie_db;
+CREATE DATABASE movie_db;
+
+\c movie_db;
+
+CREATE TABLE movies(
+    id SERIAL PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE reviews(
+    id SERIAL PRIMARY KEY,
+    review TEXT NOT NULL,
+    movie_id INTEGER NOT NULL,
+    FOREIGN KEY (movie_id) REFERENCES movies(id)
+);
