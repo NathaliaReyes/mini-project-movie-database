5f8c079a2609def2c617f708adf2d3173169668d
Silvia Reyes <104657695+NathaliaReyes@users.noreply.github.com>
2024-03-14 14:34:53 -0600

    setting up server.js, conexion created to the database in PORT 3001

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+
+const { Pool } = require('pg');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express Middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to the database:
+const pool = new Pool(
+    {
+      user: 'postgres',
+      password: 'lacasadelarbolCODING23',
+      host: 'localhost',
+      database: 'movie_db'
+    },
+    console.log(`Connected to the movie_db database.`)
+)
+
+pool.connect();
+
+
+
+app.use((req, res) => {
+    res.status(404).end();
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
