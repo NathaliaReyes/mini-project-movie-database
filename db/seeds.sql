7dfd315d16c0db19acbcd185b6404d0f34b45d86
Silvia Reyes <104657695+NathaliaReyes@users.noreply.github.com>
2024-03-14 14:03:47 -0600

    seeds.sql file added and movie_db has been seeded with data

---
+++ db/seeds.sql
@@ -0,0 +1,24 @@
+INSERT INTO movies(nombre)
+VALUES  ('The Godfather'),
+        ('The Shawshank Redemption'),
+        ('The Lord of the Rings: The Return of the King'),
+        ('Forrest Gump'),
+        ('Fight Club'),
+        ('Inception'),
+        ('The Silence of the Lambs'),
+        ('Star Wars: Episode V - The Empire Strikes Back'),
+        ('The Matrix'),
+        ('Goodfellas');
+
+INSERT INTO reviews(review, movie_id)
+VALUES  ('An epic masterpiece, one of the greatest films of all time.', 1),
+        ('A poignant tale of hope and redemption.', 2),
+        ('A spectacular conclusion to an epic trilogy.', 3),
+        ('Heartwarming and inspirational.', 4),
+        ('A mind-bending exploration of identity and consumerism.', 5),
+        ('A visually stunning and intellectually captivating journey.', 6),
+        ('A gripping psychological thriller with iconic performances.', 7),
+        ('A classic installment in the beloved Star Wars saga.', 8),
+        ('Revolutionary visual effects and a gripping story.', 9),
+        ('A gritty and captivating portrayal of organized crime.', 10);
+
