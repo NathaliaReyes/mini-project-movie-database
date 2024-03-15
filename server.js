4ce0992b72a93c9c0ad4db11790557a9ab32dc18
Silvia Reyes <104657695+NathaliaReyes@users.noreply.github.com>
2024-03-14 21:00:14 -0600

    server.js set up and package.json for dependencies needed

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const app = express();
 // Express Middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use('/api', api);
+
+app.use(express.static('public'));
 
 // Connect to the database:
 const pool = new Pool(
@@ -22,6 +25,15 @@ const pool = new Pool(
 
 pool.connect();
 
+// movies.get('/movies', (req, res) => {
+//     pool.query('SELECT * FROM movies', (err, res) => {
+//         if(err) {
+//             throw err;
+//         } else{
+//             console.log(res.rows);
+//         }
+//     })
+// })
 
 
 app.use((req, res) => {
